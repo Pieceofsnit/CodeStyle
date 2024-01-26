a06959e761aa88578fde7ac87e54a27dd9e2f4ed
Pieceofsnit <102411114+Pieceofsnit@users.noreply.github.com>
2024-01-26 17:19:49 +0500

    CodeStyle

---
+++ Scripts/AutomaticBurst.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AutomaticBurst : MonoBehaviour
+{
+    [SerializeField] private Bullet _prefabBullet;
+    [SerializeField] private Transform _shootPoint;
+    [SerializeField] private Transform _target;
+    [SerializeField] private Vector3 _velocity;
+    [SerializeField] private float _timeWaitShoot;
+    [SerializeField] private float _shootForce;
+    [SerializeField] private int _amountBullet;
+
+    private void Start()
+    {   
+        StartCoroutine(BulletShot());
+    }   
+
+    private IEnumerator BulletShot()
+    {
+        var waitForSecond = new WaitForSeconds(_timeWaitShoot);
+
+        while (_amountBullet >= 0)
+        {
+            _amountBullet--;
+            Instantiate(_prefabBullet, _shootPoint.position, _shootPoint.rotation).Shoot(_shootPoint.forward * _shootForce);
+            _prefabBullet._rigidbody.AddForce(_shootPoint.transform.forward * _shootForce * Time.deltaTime);
+            yield return waitForSecond;
+        }
+    }
+
+    private void Update()
+    {
+        transform.LookAt(_target.position);
+    }
+}
